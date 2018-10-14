f86797939581933b1b66449b07eb73a713d674b0
William Holcombe <sphinxelite@Williams-iMac.local>
2018-10-13 22:02:07 -0400

    initial setup

---
+++ client/main.js
@@ -0,0 +1,34 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './templates/app/layout.html';
+import './templates/_partials/nav.html';
+import './templates/home/index.html';
+// Router.route('/', function () {
+//   this.render('layout');
+// });
+
+
+Router.route('/', function () {
+  this.render('homeIndex');
+});
+//import './templates/main.html' 
+//import './server/routes.js';
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
